24bd3421a41d306182451790c0c512c2d4c6a875
ChristianBrauerJohanssen <60613177+ChristianBrauerJohanssen@users.noreply.github.com>
2022-05-16 18:40:36 +0200

    clean up in quadrature.py

--- HousingModelFirstTry/quadrature.py
+++ HousingModelFirstTry/quadrature.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """ quadrature
 Module is copied from consav/quadrature and a typo has been corrected.
+House price shocks has been added to create_PT_shocks()
 Functions for quadrature (mostly Gauss-Hermite).
 
 """
@@ -116,25 +117,30 @@ def log_normal_gauss_hermite(sigma,n=7,mu=None):
 
     return x,w
 
-def create_PT_shocks(sigma_psi,Npsi,sigma_xi,Nxi,sigma_epsilon,Nz,gamma,pi):  #,pi=0,mu=None):
-    """ log-normal gauss-hermite nodes for permanent transitory model
+def create_PT_shocks(sigma_psi,Npsi,sigma_xi,Nxi,sigma_epsilon,Nz,gamma,pi): 
+    """ log-normal gauss-hermite nodes for permanent transitory model,
+        including house price shocks
 
     Args:
 
         sigma_psi (double): standard deviation of permanent shock
         Npsi (int): number of points for permanent shock
         sigma_xi (double): standard deviation of transitory shock
-        Nxi (int): number of points for transitory shock        
-        pi (double): probability of low income shock
-        mu (double): value of low income shock
+        Nxi (int): number of points for transitory shock 
+        sigma_epsilon (double): std. dev. of "normal times" house price shock    
+        Nz (int): number of points for "normal times" house price shock    
+        pi (double): value of large, low probability house price decrease
 
     Returns:
 
-        psi (numpy.ndarray): nodes for permanent shock of length Npsi*Nxi+1
-        psi_w (numpy.ndarray): weights for permanent shock of length Npsi*Nxi+1
-        xi (numpy.ndarray): nodes for transitory shock of length Npsi*Nxi+1
-        xi_w (numpy.ndarray): weights for transitory shock of length Npsi*Nxi+1
-        Nshocks (int): number of nodes = Npsi*Nxi+1
+        psi (numpy.ndarray): nodes for permanent shock of lengthNpsi*Nxi*Nz+1 
+        psi_w (numpy.ndarray): weights for permanent shock of length Npsi*Nxi*Nz+1 
+        xi (numpy.ndarray): nodes for transitory shock of length Npsi*Nxi*Nz+1 
+        xi_w (numpy.ndarray): weights for transitory shock of length Npsi*Nxi*Nz+1 
+        z (numpy.ndarray): nodes for house price shock of length Npsi*Nxi*Nz+1 
+        z_w (numpy.ndarray): weights for house price shock of length Npsi*Nxi*Nz+1 
+        Nshocks (int): number of nodes = Npsi*Nxi*Nz+1 
+        -- CHECK DIMENSIONS, WHY +1? /CBJO
 
     """
 
@@ -143,10 +149,10 @@ def create_PT_shocks(sigma_psi,Npsi,sigma_xi,Nxi,sigma_epsilon,Nz,gamma,pi):  #,
     xi, xi_w = log_normal_gauss_hermite(sigma_xi,Nxi)
     eps, eps_w = normal_gauss_hermite(sigma_epsilon,Nz)
 
+    # b. house price shocks
     if gamma>0:
         z = np.append(eps,eps+pi, axis=None)
         z_w = np.append((1-gamma)*eps_w, gamma*eps_w, axis=None)
-        #par.Nz = Nz*2
     else:
         z = eps
         z_w = eps_w 
